4a1468e330f7f3512d68c8fe26b033b09061f78b
Omar Hesham <oommaarr.220022@gmail.com>
2022-03-27 21:21:20 +0200

    Made database connection string use config file

--- Config/db.php
+++ Config/db.php
@@ -4,18 +4,17 @@ class Database
 {
     private static $bdd = null;
 
-    private function __construct() {
-    }
+    // private function __construct() {
+    // }
 
     /** Gets the connection object needed for any database transaction
      * 
      * @return PDO database connection object
      */
     public static function getBdd() {
-        $config = parse_ini_file(ROOT . "Config/config.ini");
         
         if(is_null(self::$bdd)) {
-            self::$bdd = new PDO("mysql:host=".$config["DB_HOST"].";dbname=".$config["DB_NAME"], $config["DB_USER"], $config["DB_PASS"]);
+            self::$bdd = new PDO("mysql:host=".$GLOBALS["config"]["DB_HOST"].";dbname=".$GLOBALS["config"]["DB_NAME"], $GLOBALS["config"]["DB_USER"], $GLOBALS["config"]["DB_PASS"]);
         }
 
         return self::$bdd;
